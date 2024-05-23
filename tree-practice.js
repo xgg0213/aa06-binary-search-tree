0e0abe3131a1e631d95dfefc63d68f64bba7d7b0
xgg0213 <m.eringan@gmail.com>
2024-05-23 15:55:53 -0700

    compeleted findMin/Max for BT

--- tree-practice.js
+++ tree-practice.js
@@ -29,16 +29,95 @@ function findMaxBST (rootNode) {
   return currentNode.val;
 }
 
+// [NOT working - need debugging] option 1: use recursion
+// function findMinBT (rootNode) {
+//   // Your code here 
+//   if (!rootNode) return null;
+//   return findMin(rootNode, rootNode.val);
+// }
+// function findMin(node, min) {
+//   if (!node) return min;
+//   if (node.val < min) min = node.val;
+
+//   findMin(node.left, min);
+//   findMin(node.right, min);
+
+//   return min;
+// }
+
+// option 2: use iteration
 function findMinBT (rootNode) {
   // Your code here 
+  if (!rootNode) return null;
+  
+  let min = rootNode.val;
+  let queue = [rootNode];
+ 
+  while (queue.length) {
+    
+    let el = queue.shift();
+
+    if (el.left) {
+      queue.push(el.left);
+      if (el.left.val < min) min = el.left.val;
+      
+    }
+
+    if (el.right) {
+      queue.push(el.right);
+      if (el.right.val < min) min = el.right.val;
+      
+    }
+
+  }
+  return min;
+
 }
 
 function findMaxBT (rootNode) {
   // Your code here 
+  if (!rootNode) return null;
+  
+  let max = rootNode.val;
+  let queue = [rootNode];
+
+  while (queue.length) {
+    let el = queue.shift();
+
+    if (el.left) {
+      queue.push(el.left);
+      if (el.left.val > max) max = el.left.val;
+    }
+
+    if (el.right) {
+      queue.push(el.right);
+      if (el.right.val > max) max = el.right.val;
+    }
+
+  }
+
+  return max;
 }
 
 function getHeight (rootNode) {
   // Your code here 
+  if (!rootNode) return -1;
+
+  // let queue = [rootNode];
+  // let h = 0;
+
+  // while (queue.length) {
+  //   let el = queue.shift();
+  //   if (el.left) {
+  //     queue.push(el.left);
+  //   }
+  //   if (el.right) {
+  //     queue.push(el.right);
+  //   }
+  //   h+=1
+  // }
+
+  // return h;
 }
 
 function balancedTree (rootNode) {
@@ -93,3 +172,18 @@ module.exports = {
     inOrderPredecessor,
     deleteNodeBST
 }
+
+
+btRoot = new TreeNode(8);
+btRoot.left = new TreeNode(2);
+btRoot.left.left = new TreeNode(4);
+btRoot.left.right = new TreeNode(5);
+btRoot.right = new TreeNode(3);
+btRoot.right.left = new TreeNode(6);
+btRoot.right.right = new TreeNode(7);
+
+console.log(findMaxBT(btRoot))//.to.equal(1);
+//console.log(btRoot);
+findMinBT(btRoot);
+
+//console.log([btRoot].shift())
